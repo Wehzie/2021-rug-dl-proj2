be61599e35dbd50085f42e8b47aa30d73351056d
Wehzie <39304339+Wehzie@users.noreply.github.com>
2021-03-26 13:34:10 +0100

    discriminator works

--- discriminator/discriminator.py
+++ discriminator/discriminator.py
@@ -37,15 +37,14 @@ def load_data():
     # shape is 1 x number of conversations
     data = np.loadtxt('./EMNLP_dataset/dialogues_text.txt', delimiter='\n', dtype=np.str, encoding='utf-8')
     num_conversations = data.shape[0]
+    data = data[:10] # NOTE: testing
     
     # tokenize each conversation
     data = [word_tokenize(conv.lower()) for conv in data]
 
     # end of conversations indicated by "__eoc__" End-Of-Conversation token
-    max_conv_len = 0
     for conv in data:
         conv[-1] = '__eoc__'
-        max_conv_len = len(conv) if max_conv_len < len(conv) else max_conv_len 
 
     model = gensim.models.Word2Vec(data, size = 100, sg = 1, min_count = 1)
     print(model)
@@ -55,19 +54,11 @@ def load_data():
         temp_conversation = []
         for token in conv:
             temp_conversation.append(model.wv[token])
-        # padding
-        #for i in range(0, max_conv_len - len(conv)):
-        #    temp_conversation.append(np.zeros(100))
         vectors.append(temp_conversation)
 
-    print(f"Max conv len {max_conv_len}")
-    print(type(vectors[0][0]))
-    print(type(vectors[0][-1]))
 
     #save_vectors(vectors)
     #save_data(data)
-    print(len((vectors[0])))
-    print(len((data[0])))
 
     return vectors, data
 
@@ -90,9 +81,8 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.main = nn.Sequential(
-            # input is 64 x 64
             nn.RNN(
-                input_size = [64, 64],
+                input_size = 100,  # size of a token vector
                 hidden_size = 100,
                 num_layers = 10,
                 nonlinearity = 'tanh',
@@ -117,7 +107,7 @@ def trainG(batch_size, label, fake_label, netG):
     label.fill_(fake_label)
     return fake
 
-def train():
+def train(vectors):
     learning_rate = 0.05
     betas = (0.9, 0.999)    # first and second momentum
     epochs = 100
@@ -127,7 +117,8 @@ def train():
     criterion = nn.BCELoss()
 
     # load data
-    train_data, test_data = load_data()
+    train_data = vectors[0:-1000]
+    test_data = vectors[-1000:]
 
     real_label = 1
     fake_label = 0
@@ -135,6 +126,7 @@ def train():
     # setup optimizer
     optimizerD = optim.Adam(netD.parameters(), lr=learning_rate, betas=betas)
 
+    # an epoch is a full iteration over the dataset
     for epoch in range(epochs):
         for i, data in enumerate(train_data, 0):
             ############################
@@ -142,12 +134,12 @@ def train():
             ###########################
             # train with real
             netD.zero_grad()
-            real_cpu = data[0].to(device)
+            real_cpu = torch.from_numpy(data[0]).to(device)
             batch_size = real_cpu.size(0)
-            label = torch.full((batch_size,), real_label,
-                            dtype=real_cpu.dtype, device=device)
+            label = torch.full((1,), real_label, dtype=real_cpu.dtype, device=device)
 
-            output = netD(real_cpu)
+            for token in real_cpu:
+                output = netD(token)
             errD_real = criterion(output, label)
             errD_real.backward()
             D_x = output.mean().item()
@@ -161,10 +153,11 @@ def train():
             errD = errD_real + errD_fake
             optimizerD.step()
 
-        torch.save(netD.state_dict(), '%s/netD_epoch_%d.pth' % (epoch))
+        #torch.save(netD.state_dict(), '/netD_epoch_%d.pth' % (epoch))
 
 def main():
-    data = load_data()
+    vectors, data = load_data()
+    train(vectors)
 
 main()
 
